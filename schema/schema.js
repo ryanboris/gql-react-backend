7695f4dfe1dae60e0c8188e462303ab03fbb9a4b
ryanboris <ryan.boris@gmail.com>
2019-01-02 22:27:13 -0500

    adds CompanyType schema and resolver function

--- schema/schema.js
+++ schema/schema.js
@@ -6,25 +6,30 @@ import {
 } from 'graphql'
 import axios from 'axios'
 
-const users = [
-  {
-    id: '23',
-    firstName: 'Bill',
-    age: 20
-  },
-  {
-    id: '11',
-    firstName: 'Sara',
-    age: 34
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
   }
-]
+})
 
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      async resolve(parent, args, ctx, info) {
+        const company = await axios.get(
+          `http://localhost:3000/companies/${parent.companyId}`
+        )
+        return company.data
+      }
+    }
   }
 })
 
